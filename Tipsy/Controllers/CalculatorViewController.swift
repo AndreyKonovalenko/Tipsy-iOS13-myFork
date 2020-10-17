7f4ad825288af6611e8456959fda30643b2fad31
AndreyKonovalenko <andreinew@mail.ru>
2020-10-17 21:57:34 +0300

    Work on calculatorVeiewController IBOtlets and IBActions

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var zerPtcButton: UIButton!
+    @IBOutlet weak var tenPtcButton: UIButton!
+    @IBOutlet weak var twentyPtcButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func setpperValueChanged(_ sender: UIStepper) {
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
